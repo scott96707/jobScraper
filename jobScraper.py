e472e359e8b86510fa6d5f7ab23032332862b304
scott96707 <scott96707@gmail.com>
2018-12-18 17:07:09 -0700

    Working program. Hardcoded search terms and # of pages

--- jobScraper.py
+++ jobScraper.py
@@ -3,38 +3,38 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get('https://www.indeed.com/jobs?q=Python&l=Denver%2C+CO&sort=date')
-
-soup = BeautifulSoup(page.text, 'html.parser')
-
+# Create csv file to collect data
 f = csv.writer(open('Jobs.csv', 'w'))
-f.writerow(['Number', 'Job', 'Company', 'Location', 'URL'])
+f.writerow(['Job Title', 'Company', 'Location', 'Posted Date', 'Summary', 'Link'])
 
 pages = []
 
+# Gather URLs for each page
 for i in range(0, 30, 10):
     url = 'https://www.indeed.com/jobs?q=Python&l=Denver%2C+CO&sort=date&start=' + str(i)
     pages.append(url)
 
+# Loop to fetch page URLs, inner loop to gather and write data
 for item in pages:
     page = requests.get(item)
     soup = BeautifulSoup(page.text, 'html.parser')
-    
-    card_list = soup.find(id="resultsCol")
-    job_name_list_items = card_list.find_all('h2', class_="jobtitle")
-    company_name_list_items = card_list.find_all('span', class_="company")
+    all_job_posts = soup.find(id="resultsCol")
+    cards = all_job_posts.find_all(class_="jobsearch-SerpJobCard")
 
-    #This just repeats 1-10, need to fix to go past 10 later
-    for j in range(1, len(job_name_list_items)+1):
-        f.writerow([j])
-
-    for job_name in job_name_list_items:
-        names = job_name.a.get_text()        
-        f.writerow([names])
-        
-
-    for company_name in company_name_list_items:
+    # Get job specifics from each job card, post to csv
+    for posts in cards:
+        job_titles = posts.a.get_text()        
+        company = posts.find('span', class_="company").get_text().strip()
+        #Location and posted_date contained in either spans or divs
         try:
-            print(company_name.a.get_text())
-        except:
-            print(company_name.get_text())
+            location = posts.find('span', class_="location").get_text()
+        except AttributeError:
+            location = posts.find('div', class_="location").get_text()
+        try:
+            posted_date = posts.find('span', class_="date").get_text()
+        except AttributeError:
+            posted_date = "No date listed"
+        summary = posts.find('span', class_="summary").get_text().strip()
+        link_root = posts.get('data-jk')
+        link = "https://www.indeed.com/viewjob?jk=" + link_root
+        f.writerow([job_titles, company, location, posted_date, summary, link])
