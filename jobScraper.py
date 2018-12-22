22bc54be39600e38c9c8321d30f0ac143169dd36
scott96707 <scott96707@gmail.com>
2018-12-22 09:45:30 -0700

    Good for now. Does what I need

--- jobScraper.py
+++ jobScraper.py
@@ -12,31 +12,36 @@ class JobScraper:
     def getKeywords(self):
         JobScraper.keywords = urllib.parse.quote_plus(input('Enter search terms: '))
 
-    def numberOfPages(self):
+    def getNumberOfPages(self):
         try:
-            JobScraper.number_of_pages = (int(input("Enter the number of pages: "))) * 10
+            JobScraper.number_of_pages = (int(input(
+                "Enter the number of pages: "))) * 10
         except ValueError:
             print("Input an integer. Try again.")
             exit()
         if JobScraper.number_of_pages > 200:
             JobScraper.number_of_pages //= 10
-            print(str(JobScraper.number_of_pages) + " pages are way too many. Try for under 20")
+            print(str(JobScraper.number_of_pages) +\
+            " pages are way too many. Try for under 20")
             exit()
 
     def getPages(self, keywords, number_of_pages):
         # Gather URLs for each page
         for i in range(0, number_of_pages, 10):
-            url = 'https://www.indeed.com/jobs?q='+ keywords +'&l=Denver%2C+CO&sort=date&start=' + str(i)
+            url = 'https://www.indeed.com/jobs?q='+ keywords +\
+                '&l=Denver%2C+CO&sort=date&start=' + str(i)
             JobScraper.pages.append(url)
 
     def parsePages(self, pages):   
         # Loop to get and write cards with job info from web pages
         f = csv.writer(open('Jobs.csv', 'w'))
-        f.writerow(['Job Title', 'Company', 'Location', 'Posted Date', 'Summary', 'Link'])
+        f.writerow(
+            ['Job Title', 'Company', 'Location',\
+             'Posted Date', 'Summary', 'Link'])
         for item in JobScraper.pages:
             try:
                 page = requests.get(item)
-            except requests.ConnectionError: 
+            except requests.ConnectionError:
                 print("Connection Error. Check your internet connection.")
                 exit()
             soup = BeautifulSoup(page.text, 'html.parser')
@@ -65,7 +70,7 @@ class JobScraper:
 def main():
     j = JobScraper()
     j.getKeywords()
-    j.numberOfPages()
+    j.getNumberOfPages()
     j.getPages(j.keywords, j.number_of_pages)
     j.parsePages(j.pages)
 
