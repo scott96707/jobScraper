6ceef381a5386b8ca473623642d9cda507dd945f
scott96707 <scott96707@gmail.com>
2019-01-04 11:43:34 -0700

    Removed prompts for pages and keywords. Keyword is now taken from cli arg, pages is hard coded

--- jobScraper.py
+++ jobScraper.py
@@ -1,49 +1,55 @@
-import time
-import csv
-import requests
-import urllib
+#! /usr/bin/python3
+
+
+import time, csv, requests, urllib, sys
 from bs4 import BeautifulSoup
 
 class JobScraper:
-    keywords = ""
-    number_of_pages = 0
-    pages = []
 
-    def getKeywords(self):
-        JobScraper.keywords = urllib.parse.quote_plus(input('Enter search terms: '))
+    def __init__(self, keywords):
+        self.keywords = keywords
+        self.number_of_pages = 2
+        self.pages = []
 
-    def getNumberOfPages(self):
-        try:
-            JobScraper.number_of_pages = (int(input(
-                "Enter the number of pages: "))) * 10
-        except ValueError:
-            print("Input an integer. Try again.")
-            exit()
-        if JobScraper.number_of_pages > 200:
-            JobScraper.number_of_pages //= 10
-            print(str(JobScraper.number_of_pages) +\
-            " pages are way too many. Try for under 20")
-            exit()
+    #def getKeywords(self):
+    #    JobScraper.keywords = urllib.parse.quote_plus(input('Enter search terms: '))
 
-    def getPages(self, keywords, number_of_pages):
+    #def getNumberOfPages(self):
+    #    try:
+    #        JobScraper.number_of_pages = (int(input(
+    #            "Enter the number of pages: "))) * 10
+    #    except ValueError:
+    #        print("Input an integer. Try again.")
+    #        exit()
+    #    if JobScraper.number_of_pages > 200:
+    #        JobScraper.number_of_pages //= 10
+    #        print(str(JobScraper.number_of_pages) +\
+    #        " pages are way too many. Try for under 20")
+    #        exit()
+
+    def getPages(self):
         # Gather URLs for each page
-        for i in range(0, number_of_pages, 10):
-            url = 'https://www.indeed.com/jobs?q='+ keywords +\
-                '&l=Denver%2C+CO&sort=date&start=' + str(i)
-            JobScraper.pages.append(url)
+        for i in range(0, self.number_of_pages, 10):
+            url = 'https://www.indeed.com/jobs?q='+ self.keywords +\
+                '&l=Lone+Tree%2C+CO&radius=15&sort=date&start=' + str(i)
+            self.pages.append(url)
 
-    def parsePages(self, pages):   
+    def parsePages(self):   
         # Loop to get and write cards with job info from web pages
         f = csv.writer(open('Jobs.csv', 'w'))
         f.writerow(
             ['Job Title', 'Company', 'Location',\
              'Posted Date', 'Summary', 'Link'])
-        for item in JobScraper.pages:
+        for item in self.pages:
             try:
                 page = requests.get(item)
             except requests.ConnectionError:
                 print("Connection Error. Check your internet connection.")
                 exit()
+            try:
+                page.raise_for_status()
+            except Exception as exc:
+                print('There was a problem getting the page: %s' % (exc))
             soup = BeautifulSoup(page.text, 'html.parser')
             all_job_posts = soup.find(id="resultsCol")
             cards = all_job_posts.find_all(class_="jobsearch-SerpJobCard")
@@ -68,11 +74,11 @@ class JobScraper:
         
 
 def main():
-    j = JobScraper()
-    j.getKeywords()
-    j.getNumberOfPages()
-    j.getPages(j.keywords, j.number_of_pages)
-    j.parsePages(j.pages)
+    j = JobScraper(sys.argv[1])
+    #j.getKeywords()
+    #j.getNumberOfPages()
+    j.getPages()
+    j.parsePages()
 
 if __name__ == "__main__":
     main()
